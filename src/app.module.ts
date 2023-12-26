79abf57b92920de6521f5d15ecf2ac916ba2c1b0
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2023-12-26 16:46:20 +0900

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
+import { AppConfigModule } from './common/config/config.module';
 
 @Module({
-  imports: [MovieModule],
+  imports: [AppConfigModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
